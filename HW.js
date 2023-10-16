63b0ad9ee9830c19b6bf9fd5d0a018afa3ff1c90
ClarkKent2056 <luckov097@gmail.com>
2023-10-16 22:12:39 +0300

    1.0.0

---
+++ HW.js
@@ -0,0 +1,10 @@
+// https://learn.javascript.ru/generators-iterators
+// download git and connect 
+
+// download node 18..
+// npm i -g npx  
+// npx create-react-app my-react-app
+
+// props
+
+// 
---
+++ helper.js
@@ -0,0 +1,9 @@
+// import export  file => file 
+
+
+
+// component - компонента це функція котра повертає розмітку.
+
+
+//? npm run word 
+//? connect with packed.json | run script | speceal word
